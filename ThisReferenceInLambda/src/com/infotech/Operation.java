8312b1b1bf5b8b88e9efcccb0ab99a069a49f64f
Kishan Kumar <Kishan.Kumar@ingramcontent.com>
2017-04-02 13:47:47 +0530

    ThisReferenceInLambda Source code

---
+++ ThisReferenceInLambda/src/com/infotech/Operation.java
@@ -0,0 +1,6 @@
+package com.infotech;
+
+@FunctionalInterface
+public interface Operation {
+	void operate(int n);
+}
---
+++ ThisReferenceInLambda/src/com/infotech/ThisReferenceInLambda.java
@@ -0,0 +1,24 @@
+package com.infotech;
+
+public class ThisReferenceInLambda {
+
+	public void process(){
+		doPerate(30, n->{
+			System.out.println(30+n);
+			System.out.println(this);
+		});
+		
+	}
+	public static void main(String[] args) {
+		int b= 90;
+		ThisReferenceInLambda.doPerate(b, n->{
+			System.out.println(b+n);
+			//System.out.println(this);
+		});
+		new ThisReferenceInLambda().process();
+	}
+
+	private static void doPerate(int i,Operation o){
+		o.operate(i);
+	}
+}
